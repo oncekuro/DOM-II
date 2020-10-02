16ab346a2d682ecffbcae0375340526825e5e27f
oncekuro <oncekuro@users.noreply.github.com>
2020-10-02 04:28:21 -0700

    10 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
 // Your code goes here
+let heading = document.querySelector(".logo-heading");
+let logo = document.querySelector(".logo-heading");
+let nav = document.querySelectorAll(".nav a");
+let home = document.querySelector(".home");
+let footer = document.querySelector(".footer");
+let button = document.querySelectorAll(".btn");
+let text = document.querySelector("body");
+
+heading.addEventListener("click", (e) => {
+  e.target.style.backgroundColor = "green";
+});
+
+window.addEventListener("load", () => {
+  alert("Double click the Logo!!");
+});
+
+logo.addEventListener("dblclick", () => {
+  alert("Calm down!! :D");
+});
+
+nav.forEach((a) => {
+  a.addEventListener("mouseenter", () => {
+    a.style.fontWeight = "600";
+  });
+});
+
+window.addEventListener("drag", () => {
+  home.style.background = "purple";
+});
+
+window.addEventListener("scroll", () => {
+  footer.style.background = "cyan";
+});
+
+button.forEach((btn) => {
+  btn.addEventListener("wheel", () => {
+    btn.style.background = "black";
+  });
+});
+
+window.addEventListener("keydown", () => {
+  console.log("a key was pressed!");
+});
+
+window.addEventListener("resize", (e) => {
+  e.stopPropagation();
+  console.log("Window size is changing!");
+});
+
+text.addEventListener("copy", (e) => {
+  e.target.style.color = "yellow";
+});
